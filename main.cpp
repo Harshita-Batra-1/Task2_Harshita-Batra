0261e8b42c4e1992a04a20dc3fcf4cf162f1d35e
Harshita-Batra-1 <harshita0587.be21@chitkara.edu.in>
2022-05-22 13:55:18 +0530

    Initial

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl; //to print string ".."
+
+    char d = 'x'; //ending with ; vvv imp.
+    cout << d << endl; //to print char character is given as '..'
+
+    int a = 10;
+    cout<<a<< endl; //to print integer simply a bcz usko memory allocate ho chuki
+
+    bool e = false; //case sensitive False - error
+    cout<<e;
+
+    return 0;
+}
