b2f9ec261dd6b730a7c071a297532ae82390e3c6
AreenSiwach <101615440+AreenSiwach@users.noreply.github.com>
2022-05-22 23:24:26 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,12 +7,19 @@ int main()
 {
     cout << "Hello world!" << endl; 
 
-    char d = 'x';
+    char d = 'x'; //character
     cout << d << endl; 
 
-    int a = 10;
-    cout<<a<< endl; 
-    bool e = false; 
+    int a = 10; //integer
+    cout<<a<< endl;
+    
+    float b=64.89;//float
+    cout<<b<<endl;
+    
+    double v=134.64534;//double
+    cout<<v<<endl;
+    
+    bool e = false; //bool 
     cout<<e;
 
     return 0;
